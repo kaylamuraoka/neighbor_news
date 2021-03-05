817932bd6141c17fb99b1534921b0e864ff05401
kaylamuraoka <kmurs98@gmail.com>
2021-03-04 18:21:59 -1000

    adding a messages page

---
+++ client/src/pages/Messages.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the Messages Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam neque
+          consequuntur asperiores quae quia voluptatem nobis pariatur sed rem
+          dolorum a, iste nemo delectus maiores minus! Consequuntur quaerat
+          excepturi asperiores!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
