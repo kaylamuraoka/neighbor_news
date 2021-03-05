124de78b0d673313b9cccdeccee5d869b8a38223
kaylamuraoka <kmurs98@gmail.com>
2021-03-04 18:17:45 -1000

    adding Browser router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ProductListings from "./pages/ProductListings";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/product-listings" component={ProductListings} />
+      </Wrapper>
+      <Footer />
+    </Router>
   );
 }
 
