93f9d4ea00d173fc7e4bdeac8f46a37d0e8e22bb
christiansuchoski <christiansuchoski@gmail.com>
2021-03-06 10:49:15 -0800

    added to htmlRouter.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+module.exports = (app) => {
+    app.get("/exercise", (req, res) => {
+        console.log (req,)
+        res.sendFile(path.join("))
+    })
+
+    app.get("/index", (req, res) => {
+        res.sendFile(path.join("))
+    })
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join())
+    })
+}
