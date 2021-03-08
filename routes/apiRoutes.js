2f22d1924bf88e30ee32595be1bb32fd0baf2c23
Jayrich <jrkrichards@gmail.com>
2021-03-08 03:53:33 -0800

    adjusting the API routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,29 +12,29 @@ router.route("/test").delete(blogTest)
 
 router.route("/blog").get(blogGetAll).post(postBlog);
 
-// router.route("/blog/:id").get(blogFindOne).patch(blogUpdateOne).delete(blogDeleteOne);
+router.route("/blog/:id").get(blogFindOne).patch(blogUpdateOne).delete(blogDeleteOne);
 
-router.route("/blog/:id").get(function(req,res) {
-    res.send(req.params.id)
-});
+// router.route("/blog/:id").get(function(req,res) {
+//     res.send(req.params.id)
+// });
 
 
-router.post('/upload', async (req, res) => {
-    try {
-      const newImage = new Image({
-        imageUrl: req.body.imageUrl
-      });
-      await newImage.save();
-      res.json(newImage.imageUrl);
-    } catch (err) {
-      console.error('Something went wrong', err);
-    }
-  });
-  ​
-  router.get('/product-listings', async (req, res) => {
-    const getImage = await Image.findOne().sort({ _id: -1 });
-    res.json(getImage.imageUrl);
-  });
+// router.post('/upload', async (req, res) => {
+//     try {
+//       const newImage = new Image({
+//         imageUrl: req.body.imageUrl
+//       });
+//       await newImage.save();
+//       res.json(newImage.imageUrl);
+//     } catch (err) {
+//       console.error('Something went wrong', err);
+//     }
+//   });
+//   ​
+//   router.get('/product-listings', async (req, res) => {
+//     const getImage = await Image.findOne().sort({ _id: -1 });
+//     res.json(getImage.imageUrl);
+//   });
 
 
 module.exports = router;
