6c5ff07ee53eee36cc7d360c76ab19a15a4577ea
Jayrich <jrkrichards@gmail.com>
2021-03-08 03:52:38 -0800

    updating schemas so they align with the neighbor database.

--- models/imgSchema.js
+++ models/imgSchema.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const db = 'mongodb://localhost/product-listings';
-mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('Connected to MongoDB ...');
-  })
-  .catch(err => {
-    console.error('Could not Connect to MongoDb !!!', err);
-  });
+
+const ImageSchema = new Schema({
+    // Creating an image schema
+    imageUrl: {
+        type: String,
+        required: true
+    },
+});
+
+const User = mongoose.model("user", UserSchema);
 ​
 const Image = mongoose.model(
   "image",
