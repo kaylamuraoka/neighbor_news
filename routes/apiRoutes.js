f68b61e9454116fcca1b5aec752c649dd28333b0
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-03-09 21:55:30 -0800

    had to recomment out

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Blog = require("../models/blogSchema");
 const { cloudinary } = require("../utils/cloudinary");
+const express = require("express");
 
 const {
   blogTest,
@@ -21,30 +22,6 @@ router
   .patch(blogUpdateOne)
   .delete(blogDeleteOne);
 
-router.post("/product-listings"),
-  async (req, res) => {
-    try {
-      const fileStr = req.body.data;
-      const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
-        upload_preset: "neighbor_news",
-      });
-      console.log(uploadedResponse);
-      res.json({ msg: "img successfully uploaded" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ err: "img not uploaded" });
-      const publicIds = resources.map((file) => file.public_id);
-      res.send(publicIds);
-    }
-  };
-
-router.get("/api/images", async (req, res) => {
-  const { resources } = await cloudinary.search
-    .expression("folder:neighbor_news")
-    .sort_by("public_id", "desc")
-    .max_results(30)
-    .execute();
-  const publicIds = resources.map((file) => file.public_id);
-});
+// const app = express();
 
 module.exports = router;
