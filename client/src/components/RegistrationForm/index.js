1b23db80e613438072945ad78f61ec4868a42a11
kaylamuraoka <kmurs98@gmail.com>
2021-03-11 19:35:28 -1000

    adding registration form

---
+++ client/src/components/RegistrationForm/index.js
@@ -0,0 +1,214 @@
+import React, { useRef } from "react";
+import Container from "./../Container";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCamera } from "@fortawesome/free-solid-svg-icons";
+const imgPath = process.env.PUBLIC_URL + "/img/";
+
+function RegistrationForm() {
+  const profileImgInput = useRef(null);
+
+  return (
+    <Container>
+      <div className="row">
+        <h2 className="text-center">Create Account</h2>
+        <hr />
+        <form
+          className="signup pt-1"
+          method="POST"
+          enctype="multipart/form-data"
+        >
+          {/* Upload Image Section */}
+          <div className="d-flex justify-content-center">
+            <div id="profile-container">
+              <img
+                id="profileImage"
+                src={`${imgPath}default_avatar.png`}
+                alt="Avatar"
+              />
+              <div className="icon-container">
+                <div className="camera-icon">
+                  <FontAwesomeIcon icon="fa-camera" />
+                </div>
+              </div>
+            </div>
+            <input
+              ref={profileImgInput}
+              id="profile-img-input"
+              type="file"
+              name="profile_image"
+              placeholder="Profile Image"
+              accept="image/png, image/jpeg"
+            />
+          </div>
+          <small
+            className="form-text text-center text-muted"
+            id="profile-img-validation"
+          >
+            Choose Image
+          </small>
+
+          {/* Name input */}
+          <div className="form-row">
+            <div className="form-group col-6">
+              <label for="firstName-input">First Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                id="firstName-input"
+                required
+              />
+              <small id="first-name-validation" className="form-text"></small>
+            </div>
+            <div className="form-group col-6">
+              <label for="lastName-input">Last Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                id="lastName-input"
+                required
+              />
+              <small id="last-name-validation" className="form-text"></small>
+            </div>
+          </div>
+          <div className="form-row">
+            {/* Email Input */}
+            <div className="form-group col-7">
+              <label for="email-input">Email Address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email-input"
+                placeholder="Email Address"
+                required
+              />
+              <small id="email-validation" className="form-text"></small>
+            </div>
+            {/* Phone Input  */}
+            <div className="form-group col-5">
+              <label for="phone-input">Phone Number</label>
+              <input
+                type="tel"
+                className="form-control"
+                id="phone-input"
+                pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+                placeholder="Phone Number"
+                required
+              />
+              <small id="phone-validation" className="form-text"></small>
+            </div>
+          </div>
+
+          {/* Street Address input of user */}
+          <div className="form-group">
+            <label for="street-address-input">Street Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="street-address-input"
+              placeholder="Street Address"
+              required
+            />
+          </div>
+          <div className="form-row">
+            {/* City of user */}
+            <div className="form-group col-4">
+              <label for="city-input">City</label>
+              <input
+                type="text"
+                className="form-control"
+                id="city-input"
+                placeholder="City"
+                required
+              />
+            </div>
+
+            {/* State of user */}
+            <div className="form-group col-2">
+              <label for="state-input">State</label>
+              <input
+                type="text"
+                className="form-control"
+                id="state-input"
+                placeholder="State"
+                required
+              />
+            </div>
+
+            {/* Zipcode of user */}
+            <div className="form-group col-4">
+              <label for="zipcode-input">Zipcode</label>
+              <input
+                type="text"
+                className="form-control"
+                id="zipcode-input"
+                placeholder="Zipcode"
+                required
+              />
+            </div>
+
+            {/* Country of user */}
+            <div className="form-group col-2">
+              <label for="country-input">Country</label>
+              <input
+                type="text"
+                className="form-control"
+                id="country-input"
+                placeholder="Country"
+                required
+              />
+            </div>
+          </div>
+
+          {/* Password Input  */}
+          <div className="form-group">
+            <label for="password-input">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password-input"
+              placeholder="Password"
+              required
+            />
+            {/* Strong Password Validation */}
+            <small id="pwd-validation" className="form-text text-muted">
+              Your password must be 8-20 characters long, contain letters and
+              numbers, and must not contain spaces.
+            </small>
+          </div>
+          {/* Confirm Password Input */}
+          <div className="form-group">
+            <label for="confirm-password-input">Confirm Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="confirm-password-input"
+              placeholder="Confirm Password"
+              required
+            />
+            <small id="confirm-pwd-validation" className="form-text"></small>
+          </div>
+          <br />
+          <span className="text-black-50">
+            By clicking <b>"Submit"</b>, you agree to the storage and handling
+            of your data by this website in accordance with our
+            <a href="#"> Privacy Policy</a>
+          </span>
+          <br />
+          <br />
+          <button type="submit" className="btn btn-default">
+            Submit
+          </button>
+        </form>
+        <br />
+        <p>
+          Already a member? Log in <a href="/login">here</a>
+        </p>
+      </div>
+    </Container>
+  );
+}
+
+export default RegistrationForm;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
-import Container from "./../components/Container";
+// import Container from "./../components/Container";
+import RegistrationForm from "./../components/RegistrationForm";
 
 class Register extends Component {
   render() {
-    return (
-      <Container>
-        <h1>
-          This is the Registration Page so a user can create a new account
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam neque
-          consequuntur asperiores quae quia voluptatem nobis pariatur sed rem
-          dolorum a, iste nemo delectus maiores minus! Consequuntur quaerat
-          excepturi asperiores!
-        </p>
-      </Container>
-    );
+    return <RegistrationForm />;
   }
 }
 
