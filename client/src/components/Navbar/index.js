5581134ab23d607885fca5e98004327d6ff84777
kaylamuraoka <kmurs98@gmail.com>
2021-03-18 18:33:09 -1000

    adding styling to nav bar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,39 +1,43 @@
 import React from "react";
 import "./style.css";
 import NavbarLink from "../NavbarLink";
+import NavIcon from "../NavIcon";
 import { TiMessages } from "react-icons/ti";
+import { BsBell } from "react-icons/bs";
 import {
   AiOutlineTags,
   AiOutlineHome,
   AiOutlineFolderAdd,
 } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import SearchBar from "../SearchBar";
 
 function Navbar(props) {
   return (
-    <nav className="topnav">
-      <NavbarLink label="Home" path="/" icon={<AiOutlineHome />} />
-      <NavbarLink label="Messages" path="/messages" icon={<TiMessages />} />
-      <NavbarLink
-        label="Upload Listing"
-        path="/upload"
-        icon={<AiOutlineFolderAdd />}
-      />
-      <NavbarLink
-        label="Product List"
-        path="/productlist"
-        icon={<AiOutlineTags />}
-      />
-      {!props.user.user ? (
-        <>
-          <NavbarLink label="Login" path="/login" />
-          <NavbarLink label="Register" path="/register" />
-        </>
-      ) : (
-        <>
-          <NavbarLink label="Logout" path="/login" onClick={props.onClick} />
-          <NavbarLink label="My Account" path="/account" />
-        </>
-      )}
+    // {!props.user.user ? (
+    //   <>
+    //     <NavbarLink label="Login" path="/login" />
+    //     <NavbarLink label="Register" path="/register" />
+    //   </>
+    // ) : (
+    //   <>
+    //     <NavbarLink label="Logout" path="/login" onClick={props.onClick} />
+    //     <NavbarLink label="My Account" path="/account" />
+    //   </>
+    // )}
+
+    <nav className="navbar navbar-expanded navbar-light bg-white">
+      <span className="sidebar-toggle d-flex mr-2">
+        <GiHamburgerMenu className="hamburger" />
+      </span>
+      <SearchBar placeholder="Search for products..." />
+      <div class="navbar-links-div">
+        <NavIcon path="/" icon={<AiOutlineHome />} />
+        <NavIcon path="/messages" icon={<TiMessages />} count="4" />
+        <NavIcon path="/alerts" icon={<BsBell />} count="2" />
+        <NavIcon path="/upload" icon={<AiOutlineFolderAdd />} />
+        <NavIcon path="/productlist" icon={<AiOutlineTags />} count="3" />
+      </div>
     </nav>
   );
 }
