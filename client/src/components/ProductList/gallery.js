3e67c50e643dbaf4ff99a668312959c641d67c53
kaylamuraoka <kmurs98@gmail.com>
2021-03-20 04:46:57 -1000

    adding styling to product listing page

--- client/src/components/ProductList/gallery.js
+++ client/src/components/ProductList/gallery.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import API from "./../../utils/API";
+import { BsTrashFill, BsPlusCircleFill } from "react-icons/bs";
+import { AiFillEye } from "react-icons/ai";
+import { Jumbotron, Container, Button, Card } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const Gallery = () => {
+  const history = useHistory();
   const [imageIds, setImageIds] = useState([]);
   const [userData, setUserData] = useState([]);
 
@@ -46,8 +51,25 @@ const Gallery = () => {
   }, []);
 
   return (
-    <div className="cool">
-      <h1>Product Listing</h1>
+    <Container fluid>
+      <Jumbotron className="text-center">
+        <h1>Product Listings</h1>
+        <p>Check out all the products listed for sale in your neighborhood</p>
+        <hr />
+        <p>
+          <Button
+            variant="primary"
+            onClick={() => {
+              history.push("/upload");
+            }}
+          >
+            List a product
+            <BsPlusCircleFill
+              style={{ marginLeft: "8px", marginBottom: "3px" }}
+            />
+          </Button>
+        </p>
+      </Jumbotron>
 
       {userData &&
         userData.map((data, index) => {
@@ -57,24 +79,48 @@ const Gallery = () => {
                 <img src={data.imgUrl} className="card-img-top" alt="product" />
               </a>
               <div className="card-body">
-                <h5 className="card-title">{data.title}</h5>
-                <div className="card-text">
-                  {data.text}
-                  <br></br>${data.price}
-                  <br></br>Uploaded By:{data.displayName}
-                </div>
-                <button
-                  id={data._id}
-                  className="btn btn-default btn-primary"
-                  onClick={deleteProduct}
+                <h5
+                  className="card-title text-center"
+                  style={{ textTransform: "capitalize" }}
                 >
-                  Delete
-                </button>
+                  {data.title}
+                </h5>
+                <div className="card-text">
+                  <strong>Description: </strong>
+                  {data.text}
+                  <br></br>
+                  <strong>Price:</strong> ${data.price}
+                  <br></br>
+                  <p style={{ textTransform: "capitalize" }}>
+                    <strong>Seller:</strong> {data.displayName}
+                  </p>
+                </div>
+                <div className="ml-auto">
+                  <Button
+                    variant="danger"
+                    size="sm"
+                    id={data._id}
+                    onClick={deleteProduct}
+                    className="mr-1"
+                  >
+                    {" "}
+                    Delete{" "}
+                    <BsTrashFill
+                      style={{ marginLeft: "3px", marginBottom: "3px" }}
+                    />
+                  </Button>
+                  <Button variant="info" size="sm">
+                    View{" "}
+                    <AiFillEye
+                      style={{ marginLeft: "3px", marginBottom: "3px" }}
+                    />{" "}
+                  </Button>
+                </div>
               </div>
             </div>
           );
         })}
-    </div>
+    </Container>
   );
 };
 
