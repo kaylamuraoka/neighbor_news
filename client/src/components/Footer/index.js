4d75323c3649ddbb8ae6ecff2041af68a6c0042f
Jayrich <jrkrichards@gmail.com>
2021-03-23 19:24:24 -0700

    fixing footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer py-4 bg-dark text-white">
       <div className="container text-center">            
-        <small>Copyright &copy; 2020</small>
+        <small>Copyright &copy; 2021</small>
       </div>
     </footer>
   );
