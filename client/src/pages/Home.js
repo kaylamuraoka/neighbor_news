892f813368f7d007603ca015d7e4c01ac0af98b8
Jayrich <jrkrichards@gmail.com>
2021-03-23 19:23:21 -0700

    deleted excess test and made footer sticky.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,9 +14,7 @@ function Home(props) {
       ) : (
         <Banner
           backgroundImage="https://i.pinimg.com/564x/87/f5/0a/87f50a74c7fe20e185045d65492a9052.jpg"
-          title="Welcome"
-          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-          neque consequuntur"
+          title="Welcome Neighbor!"
         ></Banner>
       )}
     </div>
